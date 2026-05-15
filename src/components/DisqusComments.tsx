a428e97ba5a9a47db12993a10766a9b60e19e156
liang6328 <liang6328@gmail.com>
2026-05-15 08:02:46 +0000

    feat: use official disqus-react library for improved React integration

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -1,44 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { DiscussionEmbed } from 'disqus-react';
 import { useLocation } from 'react-router-dom';
 
 const DisqusComments: React.FC = () => {
   const location = useLocation();
-
-  useEffect(() => {
-    // This function will be called by Disqus to configure the thread
-    (window as any).disqus_config = function (this: any) {
-      this.page.url = window.location.href;
-      this.page.identifier = location.pathname;
-    };
-
-    const DISQUS = (window as any).DISQUS;
-    if (DISQUS) {
-      // If Disqus is already loaded, reset it for the new page
-      DISQUS.reset({
-        reload: true,
-        config: function (this: any) {
-          this.page.url = window.location.href;
-          this.page.identifier = location.pathname;
-        },
-      });
-    } else {
-      // If not loaded, inject the script
-      const d = document;
-      const s = d.createElement('script');
-      s.src = 'https://leon-chen.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', (+new Date()).toString());
-      (d.head || d.body).appendChild(s);
-    }
-  }, [location.pathname]);
+  const disqusShortname = 'leon-chen';
+  const disqusConfig = {
+    url: window.location.href,
+    identifier: location.pathname,
+    title: document.title || 'Eywa.ai',
+    language: 'en'
+  };
 
   return (
     <div className="w-full mt-20 border-t border-slate-800/50 pt-12 pb-20">
       <div className="max-w-4xl mx-auto px-4">
-        <div id="disqus_thread"></div>
-        <noscript>
-          Please enable JavaScript to view the{' '}
-          <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
-        </noscript>
+        <DiscussionEmbed
+          shortname={disqusShortname}
+          config={disqusConfig}
+        />
       </div>
     </div>
   );
