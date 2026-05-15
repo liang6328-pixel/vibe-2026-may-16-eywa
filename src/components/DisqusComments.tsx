65ab23a7b569c0c85149bfdfef2c76d97c9606a3
liang6328 <liang6328@gmail.com>
2026-05-15 07:56:05 +0000

    feat: integrate Disqus comments using manual script snippet for React

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -1,25 +1,40 @@
 import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const DisqusComments: React.FC = () => {
+  const location = useLocation();
+
   useEffect(() => {
+    // This function will be called by Disqus to configure the thread
+    (window as any).disqus_config = function (this: any) {
+      this.page.url = window.location.href;
+      this.page.identifier = location.pathname;
+    };
+
     const DISQUS = (window as any).DISQUS;
     if (DISQUS) {
+      // If Disqus is already loaded, reset it for the new page
       DISQUS.reset({
         reload: true,
+        config: function (this: any) {
+          this.page.url = window.location.href;
+          this.page.identifier = location.pathname;
+        },
       });
     } else {
-      const script = document.createElement('script');
-      script.src = 'https://leon-chen.disqus.com/embed.js';
-      script.setAttribute('data-timestamp', (+new Date()).toString());
-      script.async = true;
-      (document.head || document.body).appendChild(script);
+      // If not loaded, inject the script
+      const d = document;
+      const s = d.createElement('script');
+      s.src = 'https://leon-chen.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', (+new Date()).toString());
+      (d.head || d.body).appendChild(s);
     }
-  }, []);
+  }, [location.pathname]);
 
   return (
     <div className="w-full mt-20 border-t border-slate-800/50 pt-12 pb-20">
       <div className="max-w-4xl mx-auto px-4">
-        <div id="disqus_thread" className="min-h-[300px]"></div>
+        <div id="disqus_thread"></div>
         <noscript>
           Please enable JavaScript to view the{' '}
           <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
